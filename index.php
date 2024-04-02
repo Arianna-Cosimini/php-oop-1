168b01b1c4130ea91e10f4622029cc56003b3756
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-02 15:28:01 +0200

    Ho definito la classe movie e ho dichiarato le variabili d'istanza (titolo, genere, durata,autore)

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Movie {
+    public $titolo;
+    public $genere;
+    public $durata;
+    public $autore;
+}
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classi PHP</title>
+</head>
+<body>
+
+
+
+    
+</body>
+</html>
