7564985cf28fa70c10656b83db06e8ecabe5f60b
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-02 16:46:34 +0200

    definito costruttore e passato argomento agli oggetti

--- index.php
+++ index.php
@@ -5,14 +5,29 @@ class Movie {
     public $genere;
     public $durata;
     public $autore;
+
+
+    function __construct($_titolo, $_genere, $_durata, $_autore)
+    {
+        $this->titolo = $_titolo;
+        $this->genere = $_genere;
+        $this->durata = $_durata;
+        $this->autore = $_autore;
+    }
 }
 
 
 
 
+$movie1 = new Movie("Harry Potter", "Fantasy", "2 ore", " J. K. Rowling");
+$movie2 = new Movie ("Spiderman", "Fantasy", "121 minuti", "John Semper Jr.");
+$movie2 = new Movie ("Quasi amici", "commedia/drammatico", "112 minuti", " Olivier Nakache e Éric Toledano.");
+$movie2 = new Movie ("La vita è bella", "drammatico", "124 minuti", "Roberto Benigni");
 
 
 ?>
+
+
 
 
 
